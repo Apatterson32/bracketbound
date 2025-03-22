0fa7d2df7d77511bd0fd1658e11d27c912b6ac35
Alissa Patterson <135552416+Apatterson32@users.noreply.github.com>
2025-03-22 18:30:23 -0500

    Update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-orange': '#FF6B35',
+        'brand-blue': '#1A5F7A',
+        'brand-teal': '#2EC4B6',
+        'brand-gray': '#F6F7F8'
+      }
+    },
+  },
+  plugins: [],
+}
