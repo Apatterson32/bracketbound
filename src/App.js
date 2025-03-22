9d75b281003bef9f15104a2f6ec1cf2f78bd5e0a
Alissa Patterson <135552416+Apatterson32@users.noreply.github.com>
2025-03-22 18:27:34 -0500

    Update App.js from windsurf code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from './components/LandingPage';
+import './App.css';
+import './tailwind.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
